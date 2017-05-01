6c8356ddd10ba4082a618d0f8d88f2a0a104bfc1
Marcus Vinicius de Resende Maia Leite <marcus.maia.leite@gmail.com>
2017-04-30 21:24:27 -0300

    Update client_keyboard.py

--- client_keyboard.py
+++ client_keyboard.py
@@ -2,83 +2,103 @@
 
 #libraries
 import socket
-from gpiozero import Button
+from gpiozero import Button, LED
+from time import sleep
 
-#setup
+#Client setup
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
+#Keyboard & Password setup
+PASSW_LENGTH = 6
+
 #Keyboard pins
-row_1 = Button(5);  row_2 = Button(6);  row_3 = Button(13); row_4 = Button(19)
-col_1 = Button(26); col_2 = Button(16); col_3 = Button(20); col_4 = Button(21)
+row_1 = LED(5);  row_2 = LED(6);  row_3 = LED(13); row_4 = LED(19)
+col_1 = Button(26, False, 0.25); col_2 = Button(16, False, 0.25); col_3 = Button(20, False, 0.25); col_4 = Button(21, False, 0.25)
 
 #Connect to server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
 ################### Parte Keyboard ##################
-password = ''
+password = ""
 
 def add_digit(dig):
-	if len(password) == 6:
+	global password
+	if len(password) == PASSW_LENGTH - 1:
+		password = password+dig
 		#authenticate
-		s.send(password)
-		password = '';
+		s.send(str.encode(password))
+		password = "";
 		print "Autenticando..."
 	else:
 		password = password+dig
 		print password
+	sleep(0.25)
 
 #loop
 while 1:
-
 	#Catching keyboard input
-	if row_1.is_pressed:
-		if col_1.is_pressed:
-			add_digit('1')
-		elif col_2.is_pressed:
-			add_digit('2')
-		elif col_3.is_pressed:
-			add_digit('3')
-		elif col_4.is_pressed:
-			add_digit('A')
-		else:
-			print "Pressione novamente"
-	elif row_2.is_pressed:
-		if col_1.is_pressed:
-			add_digit('4')
-		elif col_2.is_pressed:
-			add_digit('5')
-		elif col_3.is_pressed:
-			add_digit('6')
-		elif col_4.is_pressed:
-			add_digit('B')
-		else:
-			print "Pressione novamente"
-	elif row_3.is_pressed:
-		if col_1.is_pressed:
-			add_digit('7')
-		elif col_2.is_pressed:
-			add_digit('8')
-		elif col_3.is_pressed:
-			add_digit('9')
-		elif col_4.is_pressed:
-			add_digit('C')
-		else:
-			print "Pressione novamente"
-	elif row_4.is_pressed:
-		if col_1.is_pressed:
-			add_digit('*')
-		elif col_2.is_pressed:
-			add_digit('0')
-		elif col_3.is_pressed:
-			add_digit('#')
-		elif col_4.is_pressed:
-			add_digit('D')
-		else:
-			print "Pressione novamente"
-	#else:
+	#Testando primeira linha
+	row_1.on()
+	row_2.off()
+	row_3.off()
+	row_4.off()
+	
+	if col_1.is_pressed:
+		add_digit("1")
+	elif col_2.is_pressed:
+		add_digit("2")
+	elif col_3.is_pressed:
+		add_digit("3")
+	elif col_4.is_pressed:
+		add_digit("A")
+	
+	#Testando segunda linha
+	row_1.off()
+	row_2.on()
+	row_3.off()
+	row_4.off()
+	
+	if col_1.is_pressed:
+		add_digit("4")
+	elif col_2.is_pressed:
+		add_digit("5")
+	elif col_3.is_pressed:
+		add_digit("6")
+	elif col_4.is_pressed:
+		add_digit("B")
+
+	#Testando terceira linha
+	row_1.off()
+	row_2.off()
+	row_3.on()
+	row_4.off()
+	
+	if col_1.is_pressed:
+		add_digit("7")
+	elif col_2.is_pressed:
+		add_digit("8")
+	elif col_3.is_pressed:
+		add_digit("9")
+	elif col_4.is_pressed:
+		add_digit("C")
+
+	#Testando quarta linha
+	row_1.off()
+	row_2.off()
+	row_3.off()
+	row_4.on()
+	
+	if col_1.is_pressed:
+		add_digit("*")
+	elif col_2.is_pressed:
+		add_digit("0")
+	elif col_3.is_pressed:
+		add_digit("#")
+	elif col_4.is_pressed:
+		add_digit("D")
 ######################################################
 	#data = s.recv(BUFFER_SIZE)
 s.close()
